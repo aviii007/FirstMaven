7209157df7be5187c0f20033e2443f72cce52291
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-02-22 00:08:57 +0530

    Initial commit

---
+++ src/main/java/com/App.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger LOG= LogManager.getLogger(App.class);
+    public static void main(String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String message="Hello World ";
+        LOG.trace(message+"will be printed on trace");
+        LOG.debug(message+"will be printed on debug");
+        LOG.info(message+"will be printed on info");
+        LOG.warn(message+"will be printed on warn");
+        LOG.error(message+"will be printed on error");
+        LOG.fatal(message+"will be printed on fatal");
+    }
+}
